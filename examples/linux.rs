0bf17c428128afae4310d205e1a48bcbe19295b9
Diego Barrios Romero <eldruin@gmail.com>
2018-08-20 17:52:37 +0200

    Add linux example

---
+++ examples/linux.rs
@@ -0,0 +1,18 @@
+extern crate embedded_hal;
+extern crate linux_embedded_hal;
+extern crate pcf857x;
+
+use linux_embedded_hal::I2cdev;
+use pcf857x::{PCF8574, SlaveAddr, PinFlags};
+
+fn main() {
+    let dev = I2cdev::new("/dev/i2c-1").unwrap();
+    let mut expander = PCF8574::new(dev, SlaveAddr::default());
+    let output_pin_status = 0b1010_1010;
+    expander.set(output_pin_status).unwrap();
+    
+    let mask_of_pins_to_be_read = PinFlags::P0 | PinFlags::P7;
+    let read_input_pin_status = expander.get(mask_of_pins_to_be_read).unwrap();
+
+    println!("Input pin status: {}", read_input_pin_status);
+}
