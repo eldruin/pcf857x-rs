94bf8237c9b50bf26d6b539126de4737d7ef62c6
Diego Barrios Romero <eldruin@gmail.com>
2018-08-20 08:34:31 +0200

    Remove test moved to common

--- tests/pcf8574.rs
+++ tests/pcf8574.rs
@@ -42,16 +42,3 @@ fn read_conserves_output_high_pins() {
     check_sent_data(expander, &[mask | write_status]);
     assert_eq!(0x01, read_status);
 }
-
-
-#[test]
-fn pin_flags_are_correct() {
-    assert_eq!(1,   PinFlags::P0);
-    assert_eq!(2,   PinFlags::P1);
-    assert_eq!(4,   PinFlags::P2);
-    assert_eq!(8,   PinFlags::P3);
-    assert_eq!(16,  PinFlags::P4);
-    assert_eq!(32,  PinFlags::P5);
-    assert_eq!(64,  PinFlags::P6);
-    assert_eq!(128, PinFlags::P7);
-}
