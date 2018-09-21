a1ea28f9b333d066fe44f1453e00f3fca17c3121
Diego Barrios Romero <eldruin@gmail.com>
2018-09-22 00:24:33 +0200

    Move u8 array / u16 conversion functions to PCF8575

--- src/devices/mod.rs
+++ src/devices/mod.rs
@@ -1,29 +1,3 @@
 pub mod pcf8574;
 pub mod pcf8575;
 mod set_pin;
-
-fn u16_to_u8_array(input: u16) -> [u8; 2] {
-    [input as u8, (input >> 8) as u8]
-}
-
-fn u8_array_to_u16(input: [u8; 2]) -> u16 {
-    input[0] as u16 | ((input[1] as u16) << 8)
-}
-
-
-#[cfg(test)]
-mod tests {
-    extern crate embedded_hal_mock as hal;
-
-    use super::*;
-
-    #[test]
-    fn can_convert_u16_to_u8_array() {
-        assert_eq!([0xCD, 0xAB], u16_to_u8_array(0xABCD));
-    }
-
-    #[test]
-    fn can_convert_u8_array_to_u16() {
-        assert_eq!(0xABCD, u8_array_to_u16([0xCD, 0xAB]));
-    }
-}
